5c0229519998a4faab45ef28375ec475ef2c1c94
Ankit <iankit17@gmail.com>
2021-09-10 15:28:30 +0530

    Updates

---
+++ precache-manifest.13284146550597240642cbfb1c5e22cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8dd2d382134839a340d77443ad6bbc",
+    "url": "/people-of-got-react/index.html"
+  },
+  {
+    "revision": "986606cb9713a9529193",
+    "url": "/people-of-got-react/static/css/2.c46e0e89.chunk.css"
+  },
+  {
+    "revision": "880bc2eeec958a95ad0a",
+    "url": "/people-of-got-react/static/css/main.1eee24dc.chunk.css"
+  },
+  {
+    "revision": "986606cb9713a9529193",
+    "url": "/people-of-got-react/static/js/2.b56847bb.chunk.js"
+  },
+  {
+    "revision": "880bc2eeec958a95ad0a",
+    "url": "/people-of-got-react/static/js/main.518f3738.chunk.js"
+  },
+  {
+    "revision": "58743fb51fcc6ef1f41e",
+    "url": "/people-of-got-react/static/js/runtime~main.7002cd67.js"
+  }
+]);
